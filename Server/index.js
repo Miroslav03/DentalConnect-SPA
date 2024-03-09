8e66576be6c013a3bb3b1dd2550f7faa1ab354f8
Miroslav Ivanov <znaka5@abv.bg>
2024-03-09 21:25:34 +0200

    Add routes

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const routes = require('./routes');
+
 const PORT = 3000;
 const app = express();
 
@@ -9,10 +11,11 @@ app.use(cors());
 app.use(express.json());
 //app.use();//auth
 
+app.use(routes)
 
 mongoose.connect('mongodb://localhost:27017/dentalconnect')
     .then(() => {
         console.log('DB is connected!');
-        app.listen(PORT, () => console.log('Server is listening on port 3030...'));
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
     })
     .catch(() => console.log('Cannot connect DB!'));
