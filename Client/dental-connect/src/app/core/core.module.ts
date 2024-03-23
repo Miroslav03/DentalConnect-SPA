15e9802eb95156e5dd25679857da9671dd4d8108
Miroslav Ivanov <znaka5@abv.bg>
2024-03-23 18:14:31 +0200

    Add about page and some routing

--- Client/dental-connect/src/app/core/core.module.ts
+++ Client/dental-connect/src/app/core/core.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { UserRoutingModule } from '../user/user-routing.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    UserRoutingModule
   ],
-  exports:[HeaderComponent]
+  exports: [HeaderComponent, AboutComponent, HomeComponent, ErrorPageComponent]
 })
 export class CoreModule { }
