04a71a71834b9cd9710d1031c457f078dc77a2ea
Miroslav Ivanov <znaka5@abv.bg>
2024-03-26 21:14:07 +0200

    Changing html css entirely

---
+++ Client/dental-connect/src/app/services/services.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllServicesComponent } from './all-services/all-services.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllServicesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServicesModule { }
