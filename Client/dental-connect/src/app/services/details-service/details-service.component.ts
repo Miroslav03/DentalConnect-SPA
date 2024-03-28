7b40881e61cb4c3423fd59907b9b872d2f1b910f
Miroslav Ivanov <znaka5@abv.bg>
2024-03-28 14:20:39 +0200

    New Details page and profile fixes

--- Client/dental-connect/src/app/services/details-service/details-service.component.ts
+++ Client/dental-connect/src/app/services/details-service/details-service.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-details-service',
   templateUrl: './details-service.component.html',
-  styleUrls: ['./details-service.component.css']
+  styleUrls: ['./details-service.component.scss']
 })
 export class DetailsServiceComponent {
 
